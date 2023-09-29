dffec336d80e1a65b56026c5d8a9c2220126132f
elsergio130v <59649728+elsergio130v@users.noreply.github.com>
2023-09-29 16:31:51 +0200

    acabado el programa
    una pagina web con una cuenta atras hasta el proximo cumpleaños que le cambia la imagen de fondo dependiendo de la estación en la que nos encontramos y cuando es el cumpleaños.

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+var img = document.createElement('img');
 const diasElement = document.getElementById('dias');
 const hoursElement = document.getElementById('horas');
 const minsElement = document.getElementById('min');
 const secondsElement = document.getElementById('seg');
 const upcomingImg = document.getElementById('upcoming-img');
+const fondoImg = document.getElementById('fondos');
+
+const diaEstacionPriVer = 21;
+const diaEstacionOtoño = 23;
+const diaEstacionInvierno = 22;
+const mesEstacionPrimavera = 3;
+const mesEstacionVerano = 6;
+const mesEstacionOtono = 9;
+const mesEstacionInvierno = 12;
 
 let birthdias = new Date("2024-09-03");
 let totalSeg,dias,horas,min,segundos;
@@ -16,8 +26,11 @@ let countdownInterval = setInterval(cuentaAtras, 1000);
 cuentaAtras();
 
 function cuentaAtras() {
-    fechaActual = new Date();
-    totalSeg = (birthdias - fechaActual) / 1000;
+    var fechaActual = new Date();
+    if (fechaActual.getMonth) {
+      
+    }
+    totalSeg = (birthdias - fechaActual) / 1000;    
   
     // Condición para comprobar si ha llegado la hora establecida
     if (Math.floor(totalSeg) <= 0) {
@@ -43,9 +56,59 @@ function cuentaAtras() {
     hoursElement.innerHTML = "  Horas - "+horas;
     minsElement.innerHTML = " min - " + min;
     secondsElement.innerHTML = " sec - "+segundos;
-  
+    comprobarEstacion();
   };
+  
+  function showProduct() {
+    clearInterval(countdownInterval);
+  }
+
+  function comprobarEstacion() {  
+    var diaActual =  new Date();
+    var dia = diaActual.getDate();
+    var mes = diaActual.getMonth()+1;
+    var mesCumple = birthdias.getMonth()+1;
+    var diaCumple = birthdias.getDate();
+
+    let estacion = '';
+
+    if ((mes == 3 && dia >= 21) || (mes > 3 && mes < 6) || (mes == 6 && dia < 21)) {
+        console.log("Primavera");
+        document.body.style.backgroundImage = "url('img/primavera.jpg')";
+        optimizartImg();
+      } else if ((mes == 6 && dia >= 21) || (mes > 6 && mes < 9) || (mes == 9 && dia < 22)) {
+        console.log("Verano");
+        document.body.style.backgroundImage = "url('img/verano.jpg')";
+        optimizartImg();
+      } else if ((mes == 9 && dia >= 22) || (mes > 9 && mes < 12) || (mes == 12 && dia < 21)) {
+        console.log("Otoño");
+        document.body.style.backgroundImage = "url('img/otono.jpg')";
+        optimizartImg();
+        
+      } else {
+      console.log("invierno");
+      document.body.style.backgroundImage = "url('img/invierno.jpg')";
+      optimizartImg();
+    }if (mes == mesCumple && dia == diaCumple) {
+      showProduct();
+    
+    console.log("dia: " + dia+" mes: " + mes);
+    
 
 
+  }
+}
 
+function optimizartImg(){
+        document.body.style.backgroundPosition = 'center';
+        document.body.style.backgroundRepeat = 'no-repeat';
+        document.body.style.backgroundSize = 'cover';
+        document.body.style.backgroundAttachment = 'fixed';
 
+}
+  function showProduct(){
+    clearInterval(countdownInterval);
+  var img = document.createElement('img');
+  img.src = 'img/cumple.jpg';
+  document.body.appendChild(img);
+  }
